8e6caa42e1fd4f03109bfd274dac70cbbbee120c
Raul Ferreira <105366476+RaulFerreiraSilva@users.noreply.github.com>
2024-01-25 12:01:09 -0300

    feat: Add Snorlax-Mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,16 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   title = 'PokeDesk';
-  constructor() {}
+  constructor() {
+  }
 
+  snorlaxDex(): void {
+    document.querySelectorAll('*').forEach((tag) => {
+      if(tag.classList.contains('snorlax')){
+        tag.classList.remove('snorlax')
+      }else{
+        tag.classList.add('snorlax')
+      }
+    });
+  }
 }
